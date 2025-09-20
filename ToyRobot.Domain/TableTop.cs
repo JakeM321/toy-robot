996c86ad33bc333da1c682c2a23481b710dfe84e
Jake Marshall <jake.marshall123@outlook.com>
2025-09-20 20:41:43 +1000

    Cleanup

--- ToyRobot.Domain/TableTop.cs
+++ ToyRobot.Domain/TableTop.cs
@@ -2,18 +2,15 @@
 
 internal class TableTop(int width, int height) : IMovementCoordinator
 {
-    private int _width = width;
-    private int _height = height;
-
     public Robot PlaceRobot(int posX, int posY, Direction directionFacing)
     {
-        throw new NotImplementedException();
+        return new Robot(this, posX, posY, directionFacing);
     }
 
     public bool IsMoveLegal(int x, int y)
     {
-        var xPosMax = _width - 1;
-        var yPosMax = _height - 1;
+        var xPosMax = width - 1;
+        var yPosMax = height - 1;
 
         return x <= xPosMax
             && x >= 0
