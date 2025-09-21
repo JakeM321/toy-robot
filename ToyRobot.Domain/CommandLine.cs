768e17e7ace12b1dbf7466d86e2c605cffedf859
Jake Marshall <jake.marshall123@outlook.com>
2025-09-21 13:12:15 +1000

    Cleans up CommandLine implementation

--- ToyRobot.Domain/CommandLine.cs
+++ ToyRobot.Domain/CommandLine.cs
@@ -13,66 +13,83 @@ public class CommandLine
     {
         var portions = prompt.Split(' ');
         var command = portions[0].ToUpper();
-        if (command == Constants.Commands.Place)
+        switch(command)
         {
-            (var coordinates, string error) = ValidatePlaceCommand(portions);
-            if (coordinates == null)
-                return error;
-            var result = _session.Place(coordinates);
-            if (result == CommandResult.OutOfBounds)
-                return Constants.Messages.CannotPlaceOutsideOfGrid;
-            return Constants.Messages.Ok;
+            case Constants.Commands.Place:
+                return HandlePlaceCommand(portions);
+            case Constants.Commands.Move:
+                return HandleMoveCommand();
+            case Constants.Commands.Left:
+                return HandleLeftCommand();
+            case Constants.Commands.Right:
+                return HandleRightCommand();
+            case Constants.Commands.Report:
+                return HandleReportCommand();
+            default:
+                return string.Format(Constants.Messages.CommandNotRecognised, command);
         }
-
-        if (command == Constants.Commands.Move)
-        {
-            var result = _session.Move();
-            if (result == CommandResult.OutOfBounds)
-                return Constants.Messages.CannotMove;
-            if (result == CommandResult.InitialPlacementMissing)
-                return  Constants.Messages.CannotMoveRobotInitialPlacementMissing;
-            return Constants.Messages.Ok;
-        }
-
-        if (command == Constants.Commands.Left)
-        {
-            var result = _session.Left();
-            if (result == CommandResult.InitialPlacementMissing)
-                return Constants.Messages.CannotMoveRobotInitialPlacementMissing;
-            return Constants.Messages.Ok;
-        }
-
-        if (command == Constants.Commands.Right)
-        {
-            var result = _session.Right();
-            if (result == CommandResult.InitialPlacementMissing)
-                return Constants.Messages.CannotMoveRobotInitialPlacementMissing;
-            return Constants.Messages.Ok;
-        }
-
-        if (command == Constants.Commands.Report)
-        {
-            var result = _session.Report();
-            if (result == null)
-                return Constants.Messages.CannotReportInitialPlacementMissing;
-            switch (result.FDirection)
-            {
-                case Direction.North:
-                    return $"{result.XPosition},{result.YPosition},{Constants.Directions.North}";
-                case Direction.East:
-                    return $"{result.XPosition},{result.YPosition},{Constants.Directions.East}";
-                case Direction.South:
-                    return $"{result.XPosition},{result.YPosition},{Constants.Directions.South}";
-                case Direction.West:
-                    return $"{result.XPosition},{result.YPosition},{Constants.Directions.West}";
-            }
-
-            return Constants.Messages.Ok;
-        }
-
-        return string.Format(Constants.Messages.CommandNotRecognised, command);
     }
 
+    #region Handlers
+    private string HandlePlaceCommand(string[] portions)
+    {
+        (var coordinates, string error) = ValidatePlaceCommand(portions);
+        if (coordinates == null)
+            return error;
+        var result = _session.Place(coordinates);
+        if (result == CommandResult.OutOfBounds)
+            return Constants.Messages.CannotPlaceOutsideOfGrid;
+        return Constants.Messages.Ok;
+    }
+
+    private string HandleMoveCommand()
+    {
+        var result = _session.Move();
+        if (result == CommandResult.OutOfBounds)
+            return Constants.Messages.CannotMove;
+        if (result == CommandResult.InitialPlacementMissing)
+            return Constants.Messages.CannotMoveRobotInitialPlacementMissing;
+        return Constants.Messages.Ok;
+    }
+
+    private string HandleLeftCommand()
+    {
+        var result = _session.Left();
+        if (result == CommandResult.InitialPlacementMissing)
+            return Constants.Messages.CannotMoveRobotInitialPlacementMissing;
+        return Constants.Messages.Ok;
+    }
+
+    private string HandleRightCommand()
+    {
+        var result = _session.Right();
+        if (result == CommandResult.InitialPlacementMissing)
+            return Constants.Messages.CannotMoveRobotInitialPlacementMissing;
+        return Constants.Messages.Ok;
+    }
+
+    private string HandleReportCommand()
+    {
+        var result = _session.Report();
+        if (result == null)
+            return Constants.Messages.CannotReportInitialPlacementMissing;
+        switch (result.FDirection)
+        {
+            case Direction.North:
+                return $"{result.XPosition},{result.YPosition},{Constants.Directions.North}";
+            case Direction.East:
+                return $"{result.XPosition},{result.YPosition},{Constants.Directions.East}";
+            case Direction.South:
+                return $"{result.XPosition},{result.YPosition},{Constants.Directions.South}";
+            case Direction.West:
+                return $"{result.XPosition},{result.YPosition},{Constants.Directions.West}";
+        }
+
+        return Constants.Messages.Ok;
+    }
+    #endregion
+
+    #region Validation
     private (Coordinates?, string) ValidatePlaceCommand(string[] portions)
     {
         var arguments = string.Join(string.Empty, portions.Skip(1))
@@ -84,11 +101,11 @@ public class CommandLine
         var fDir = ValidateDirection(arguments, 3);
 
         if (xPos == null)
-            return (null, String.Format(Constants.Messages.ParameterError, Constants.Parameters.XPosition, Constants.Commands.Place));
+            return (null, string.Format(Constants.Messages.ParameterError, Constants.Parameters.XPosition, Constants.Commands.Place));
         if (yPos == null)
-            return (null, String.Format(Constants.Messages.ParameterError, Constants.Parameters.YPosition, Constants.Commands.Place));
+            return (null, string.Format(Constants.Messages.ParameterError, Constants.Parameters.YPosition, Constants.Commands.Place));
         if (fDir == null)
-            return (null, String.Format(Constants.Messages.ParameterError, Constants.Parameters.FDirection, Constants.Commands.Place));
+            return (null, string.Format(Constants.Messages.ParameterError, Constants.Parameters.FDirection, Constants.Commands.Place));
 
         var coords = new Coordinates(xPos.Value, yPos.Value, fDir.Value);
         return (coords, string.Empty);
@@ -123,6 +140,7 @@ public class CommandLine
                 return null;
         }
     }
+    #endregion
 
     public static CommandLine Create()
     {
