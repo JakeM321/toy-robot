fb9ef8ed76f30ec121ed236881bc60d4dbe08738
Jake Marshall <jake.marshall123@outlook.com>
2025-09-21 14:25:11 +1000

    Adds demonstration test

--- ToyRobot.Domain.Tests/ComponentTests.cs
+++ ToyRobot.Domain.Tests/ComponentTests.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TestStack.BDDfy.Xunit;
-
-namespace ToyRobot.Domain.Tests
+﻿namespace ToyRobot.Domain.Tests
 {
     public class ComponentTests
     {
+        // Examples from the challenge worksheet
         [Fact]
         public void ExampleCaseOne()
         {
@@ -55,6 +49,76 @@ namespace ToyRobot.Domain.Tests
 
             Submit("REPORT");
             Expect("3,3,NORTH");
+        }
+
+        // Demonstration to showcase a variety of commands and responses in a single debugging session
+        [Fact]
+        public void MainDemonstration()
+        {
+            Submit("MOVE");
+            Expect("Cannot move robot that has not yet been placed");
+
+            Submit("LEFT");
+            Expect("Cannot move robot that has not yet been placed");
+
+            Submit("PLACE 6,7,NORTH");
+            Expect("Cannot place robot outside of grid boundary");
+
+            Submit("PLACE 1,2,EAST");
+            Submit("REPORT");
+            Expect("1,2,EAST");
+
+            Submit("MOVE");
+            Submit("REPORT");
+            Expect("2,2,EAST");
+
+            Submit("MOVE");
+            Submit("REPORT");
+            Expect("3,2,EAST");
+
+            Submit("MOVE");
+            Submit("MOVE");
+            Expect("Further movement would place robot outside of grid");
+
+            Submit("REPORT");
+            Expect("4,2,EAST");
+
+            Submit("MOVE");
+            Expect("Further movement would place robot outside of grid");
+
+            Submit("LEFT");
+            Submit("REPORT");
+            Expect("4,2,NORTH");
+
+            Submit("MOVE");
+            Submit("REPORT");
+            Expect("4,3,NORTH");
+
+            Submit("MOVE");
+            Submit("REPORT");
+            Expect("4,4,NORTH");
+
+            Submit("MOVE");
+            Expect("Further movement would place robot outside of grid");
+
+            Submit("LEFT");
+            Submit("MOVE");
+            Submit("MOVE");
+            Submit("MOVE");
+            Submit("REPORT");
+            Expect("1,4,WEST");
+
+            Submit("MOVE");
+            Submit("REPORT");
+            Expect("0,4,WEST");
+
+            Submit("MOVE");
+            Expect("Further movement would place robot outside of grid");
+
+            Submit("PLACE 0,0,NORTH");
+            Submit("MOVE");
+            Submit("REPORT");
+            Expect("0,1,NORTH");
         }
 
         #region Helpers
